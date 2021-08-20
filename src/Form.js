9bf0973ff648d4f5a623acce54114d043c68276e
Your Name <pbejune22@gmail.com>
2021-08-19 22:55:56 -0400

    refactored code form data.

--- src/Form.js
+++ src/Form.js
@@ -1,55 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default class Form extends React.Component {
-    state = { 
-        fullName: "",
-        email: "",
-        password: ""
-    };
+const Form = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    change = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let formData = {};
+    formData.fullName = fullName;
+    formData.email = email;
+    formData.passowrd = password;
+    console.log(formData);
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        this.props.onSubmit(this.state);
-        this.setState({
-            fullName: "",
-            email: "",
-            password: ""
-        });
-    };
+  return (
+    <form>
+      <input id="fullName" name="fullName" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+      <br />
+      <input name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <input name="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button onClick={(e) => onSubmit(e)}>Submit</button>
+    </form>
+  );
+};
 
-    render() {
-        return (
-            <form>
-                <input 
-                name="fullName"
-                placeholder="Full Name" 
-                value={this.state.fullName} 
-                onChange={e => this.change(e)}
-                 />
-                 <br />
-                <input 
-                name="email"
-                placeholder="Email" 
-                value={this.state.email} 
-                onChange={e => this.change(e)}
-                 />
-                 <br />
-                <input 
-                name="password"
-                type="password"
-                placeholder="Password" 
-                value={this.state.password} 
-                onChange={e => this.change(e)}
-                 />
-                 <br />
-                 <button onClick={e => this.onSubmit(e)}>Submit</button>
-            </form>
-        );
-    }
-}
+export default Form;
